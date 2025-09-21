e33ebcf92b786c96e63e00c330d99c7b0b68f599
Jerameel Gonga <jerameelcode@gmail.com>
2025-09-21 03:33:25 +0100

    project running...

---
+++ java/springboot-microsservice/src/test/java/com/gonga/tcc/microsservice/MicrosserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gonga.tcc.microsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is intentionally left empty to verify that the Spring application context loads successfully.
+		// throw new UnsupportedOperationException("Test not implemented");
+	}
+
+}

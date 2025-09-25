323aafc22e81ba9903fde0c252342bb8a29c74dd
Jerameel Gonga <jerameelcode@gmail.com>
2025-09-26 00:54:26 +0100

    javascript api with nestjs

--- java/springboot-microsservice/src/main/java/com/gonga/tcc/microsservice/controller/UserController.java
+++ java/springboot-microsservice/src/main/java/com/gonga/tcc/microsservice/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,13 +26,14 @@ import com.gonga.tcc.microsservice.dtos.UserResponseDTO;
 import com.gonga.tcc.microsservice.service.UserService;
 
 
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
     private final PagedResourcesAssembler<UserResponseDTO> pagedResourcesAssembler;
+
+    private static final String ALL_USERS_REL = "all-users";
 
     public UserController(UserService userService, PagedResourcesAssembler<UserResponseDTO> pagedResourcesAssembler) {
         this.userService = userService;
@@ -44,24 +47,43 @@ public class UserController {
         return ResponseEntity.ok(pagedModel);
     }
 
-    @PostMapping("")
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<UserResponseDTO>> getUserById(@PathVariable UUID id) {
+        var user = userService.findById(id);
+        var entityModel = EntityModel.of(user,
+            linkTo(methodOn(UserController.class).getAllUsers(null)).withRel(ALL_USERS_REL),
+            linkTo(methodOn(UserController.class).getUserById(id)).withSelfRel()
+        );
+        return ResponseEntity.ok(entityModel);
+    }
+
+    @PostMapping
     public ResponseEntity<EntityModel<UserResponseDTO>> createUser(@RequestBody UserRequestDTO userRequestDTO) {
         var createdUser = userService.createUser(userRequestDTO);
-        var entityModel = EntityModel.of(createdUser);
+        var entityModel = EntityModel.of(createdUser,
+            linkTo(methodOn(UserController.class).getAllUsers(null)).withRel(ALL_USERS_REL),
+            linkTo(methodOn(UserController.class).createUser(null)).withSelfRel()
+        );
         return ResponseEntity.status(HttpStatus.CREATED).body(entityModel);
     }
     
     @PutMapping("/{id}")
     public ResponseEntity<EntityModel<UserResponseDTO>> updateUser(@PathVariable UUID id, @RequestBody UserRequestDTO userRequestDTO) {
         var updatedUser = userService.updateUser(id, userRequestDTO);
-        var entityModel = EntityModel.of(updatedUser);
+        var entityModel = EntityModel.of(updatedUser,
+            linkTo(methodOn(UserController.class).getAllUsers(null)).withRel(ALL_USERS_REL),
+            linkTo(methodOn(UserController.class).updateUser(id, userRequestDTO)).withSelfRel()
+        );
         return ResponseEntity.ok(entityModel);
     }
     
     @PatchMapping("/{id}")
     public ResponseEntity<EntityModel<UserResponseDTO>> partialUpdateUser(@PathVariable UUID id, @RequestBody Map<String, Object> updates) {
         var updatedUser = userService.partialUpdateUser(id, updates);
-        var entityModel = EntityModel.of(updatedUser);
+        var entityModel = EntityModel.of(updatedUser,
+            linkTo(methodOn(UserController.class).getAllUsers(null)).withRel(ALL_USERS_REL),
+            linkTo(methodOn(UserController.class).partialUpdateUser(id, null)).withSelfRel()
+        );
         return ResponseEntity.ok(entityModel);
     }
 }
--- java/springboot-microsservice/src/main/java/com/gonga/tcc/microsservice/service/UserService.java
+++ java/springboot-microsservice/src/main/java/com/gonga/tcc/microsservice/service/UserService.java
@@ -5,6 +5,9 @@ import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,12 @@ import com.gonga.tcc.microsservice.repository.UserRepository;
 @Service
 public class UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    @Value("${user.not.found}")
+    private String userNotFound;
+
+    private static final String LOG_MSG_FORMAT = "{}: {}";
+
     private final UserRepository userRepository;
     private static final UserMapper userMapper = UserMapper.INSTANCE;
 
@@ -27,35 +36,58 @@ public class UserService {
     }
 
     public Page<UserResponseDTO> findAll(Pageable pageable) {
+        logger.info("Fetching all users with pagination: {}", pageable);
         return userRepository.findAll(pageable).map(userMapper::toDTO);
     }
 
+    public UserResponseDTO findById(UUID id) {
+        logger.info("Fetching user with ID: {}", id);
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            logger.error(LOG_MSG_FORMAT, userNotFound, id);
+            return new ResourceNotFoundException(userNotFound+": " + id);
+        });
+        logger.info("User found with ID: {}", id);
+        return userMapper.toDTO(user);
+    }
+
     public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+        logger.info("Creating user with email: {}", userRequestDTO.getEmail());
         if (userRepository.existsByEmail(userRequestDTO.getEmail())) {
+            logger.error("Email already exists: {}", userRequestDTO.getEmail());
             throw new IllegalArgumentException("Email already exists");
         }
         var userEntity = userMapper.toEntity(userRequestDTO);
         var savedUser = userRepository.save(userEntity);
+        logger.info("User created successfully with ID: {}", savedUser.getId());
         return userMapper.toDTO(savedUser);
     }
 
     public UserResponseDTO updateUser(UUID id, UserRequestDTO userRequestDTO) {
+        logger.info("Updating user with ID: {}", id);
         User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+                .orElseThrow(() -> {
+                    logger.error(LOG_MSG_FORMAT, userNotFound, id);
+                    return new ResourceNotFoundException(userNotFound+": " + id);
+                });
 
-        user.setName(userRequestDTO.getName());
-        user.setEmail(userRequestDTO.getEmail());
-        user.setPassword(userRequestDTO.getPassword());
+        User userToCreate = userMapper.toEntity(userRequestDTO);
+        userToCreate.setId(user.getId());
 
-        User updatedUser = userRepository.save(user);
+        User updatedUser = userRepository.save(userToCreate);
+        logger.info("User updated successfully with ID: {}", updatedUser.getId());
         return userMapper.toDTO(updatedUser);
     }
 
     public UserResponseDTO partialUpdateUser(UUID id, Map<String, Object> updates) {
-        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+        logger.info("Partially updating user with ID: {}", id);
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            logger.error(LOG_MSG_FORMAT, userNotFound, id);
+            return new ResourceNotFoundException(userNotFound+": " + id);
+        });
 
         updates.forEach((key, value) -> {
             try {
+                logger.info("Updating field '{}' with value '{}'", key, value);
                 PropertyDescriptor pd = new PropertyDescriptor(key, User.class);
                 Method setter = pd.getWriteMethod();
                 if (setter != null) {
@@ -63,12 +95,17 @@ public class UserService {
                 } else {
                     throw new IllegalArgumentException("Field does not have a setter: " + key);
                 }
+            } catch (IllegalAccessException | IllegalArgumentException e) {
+                logger.error("Error while updating field '{}' for user with ID '{}': {}", key, id, e.getMessage(), e);
+                throw new IllegalArgumentException("Error while updating field: " + key + " for user with ID: " + id, e);
             } catch (Exception e) {
-                throw new IllegalArgumentException("Invalid field: " + key, e);
+                logger.error("Unexpected error while updating field '{}': {}", key, e.getMessage(), e);
+                throw new IllegalArgumentException("Unexpected error while updating field: " + key, e);
             }
         });
 
         User updatedUser = userRepository.save(user);
+        logger.info("User partially updated successfully with ID: {}", updatedUser.getId());
         return userMapper.toDTO(updatedUser);
     }
 
